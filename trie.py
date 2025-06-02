bdbf2e22dd7e1cbc2631cf6a7e382b7fb3f73d7b
SkinnyPigeon <e.blackledge@stuart.com>
2025-06-03 00:12:16 +0200

    feat/implementing-act-and-signoff (#2)

--- trie.py
+++ trie.py
@@ -5,24 +5,33 @@ It is used to store a dynamic set or associative array where the keys are usuall
 
 # This is a trie data structure implementation in Python
 
+
+# pylint: disable=too-few-public-methods
 class TrieNode:
     """
     TrieNode is a node in the trie data structure.
-    It contains a dictionary of children nodes and a boolean flag to indicate if the node is the end of a word.
+    It contains a dictionary of children nodes and a boolean
+    flag to indicate if the node is the end of a word.
     """
-    def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
+
+    def __init__(self) -> None:
+        self.children: dict[str, "TrieNode"] = {}
+        self.is_end_of_word: bool = False
+
 
 class Trie:
     """
     Trie is a tree-like data structure that stores a dynamic set of strings.
     It is used to store a dynamic set or associative array where the keys are usually strings.
     """
-    def __init__(self):
+
+    def __init__(self) -> None:
         self.root = TrieNode()
 
-    def insert(self, word):
+    def insert(self, word: str) -> None:
+        """
+        Insert a word into the trie
+        """
         node = self.root
         for char in word:
             if char not in node.children:
@@ -30,7 +39,10 @@ class Trie:
             node = node.children[char]
         node.is_end_of_word = True
 
-    def search(self, word):
+    def search(self, word: str) -> bool:
+        """
+        Search for a word in the trie
+        """
         node = self.root
         for char in word:
             if char not in node.children:
@@ -38,7 +50,10 @@ class Trie:
             node = node.children[char]
         return node.is_end_of_word
 
-    def starts_with(self, prefix):
+    def starts_with(self, prefix: str) -> bool:
+        """
+        Check if the trie starts with a given prefix
+        """
         node = self.root
         for char in prefix:
             if char not in node.children:
